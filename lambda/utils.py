8d88058a42a746a0528517c3f65dc3e62d179e3a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-10-20 14:19:04 +0000

    updated lambda/utils.py.

--- lambda/utils.py
+++ lambda/utils.py
@@ -46,5 +46,5 @@ def get_exchange_rates():
 
 
 def get_random_greating():
-    greatings_list = ["Hola", "Cómo estás", "Python es genial", "Saludos desde Cuba", "Adiós"]
+    greatings_list = ["Asere que bolá? Los precios están mandáo.", "En talla asere.", "Ufff, los precios están por las nubes.", "Saludos broder."]
     return random.choice(greatings_list)
