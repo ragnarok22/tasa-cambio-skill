345d7d2d402ecdb4d27c4b66672fa3a9503e447c
Reinier Hernández <sasuke.reinier@gmail.com>
2025-10-04 13:56:24 -0300

    refactor(lambda): add type hints and tidy imports

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
-# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-# session persistence, api calls, and more.
-# This sample is built using the handler classes approach in skill builder.
 import logging
+
 import ask_sdk_core.utils as ask_utils
-
-from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.dispatch_components.exception_components import (
+    AbstractExceptionHandler,
+)
+from ask_sdk_core.dispatch_components.request_components import AbstractRequestHandler
 from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_model.response import Response
 
-from ask_sdk_model import Response
-
-from utils import get_exchange_rates, get_random_greating
+from .utils import get_exchange_rates, get_random_greating
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -22,68 +19,64 @@ logger.setLevel(logging.INFO)
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
 
         return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
+    ask_utils.request_util.get_account_linking_access_token
+
+    def handle(self, handler_input: HandlerInput) -> Response:
         speak_output = "Qué bola asere? Dime que quieres saber?"
 
         return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
+            handler_input.response_builder.speak(speak_output)
+            .ask(speak_output)
+            .response
         )
 
 
 class ExchangeRateIntentHandler(AbstractRequestHandler):
     """Handler for Exchange Rates Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
         return ask_utils.is_intent_name("ExchangeRateIntent")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
+    def handle(self, handler_input: HandlerInput) -> Response:
         # request API to get the prices of MLC, USD and Euro
         currencies = get_exchange_rates()
-        
+
         mlc_value = round(currencies["MLC"], 2)
         usd_value = round(currencies["USD"], 2)
         eur_value = round(currencies["EUR"], 2)
-        
+
         random_greating = get_random_greating()
-        speak_output = f'{random_greating}. El M. L. C. está en {mlc_value} pesos. El U. S. D. casi en lo mismo con un valor de {usd_value} pesos. Y el Euro ni se diga. Ese anda por los {eur_value} pesos'
+        speak_output = f"{random_greating}. El M. L. C. está en {mlc_value} pesos. El U. S. D. casi en lo mismo con un valor de {usd_value} pesos. Y el Euro ni se diga. Ese anda por los {eur_value} pesos"
 
         return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
+            handler_input.response_builder.speak(speak_output)
+            # .ask("add a reprompt if you want to keep the session open for the user to respond")
+            .response
         )
 
 
 class ExchangeRateRequestIntentHandler(AbstractRequestHandler):
     """Handler for Exchange Rates Request Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
         return ask_utils.is_intent_name("ExchangeRateRequestIntent")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
+    def handle(self, handler_input: HandlerInput) -> Response:
         # request API to get the prices of MLC, USD and Euro
         currencies = get_exchange_rates()
-        
+
         mlc_value = round(currencies["MLC"], 2)
         usd_value = round(currencies["USD"], 2)
         eur_value = round(currencies["EUR"], 2)
-        
-        
+
         slots = handler_input.request_envelope.request.intent.slots
         currency_type = slots["currency"].value
-        
+
         logger.info(slots)
 
         text_output = ""
@@ -92,79 +85,73 @@ class ExchangeRateRequestIntentHandler(AbstractRequestHandler):
         elif currency_type == "euro":
             text_output = f"El Euro más caliente que el caribe. {eur_value} pesos."
         elif currency_type == "MLC":
-            text_output = f"El M. L. C. un poco por debajo del dólar a {mlc_value} pesos."
+            text_output = (
+                f"El M. L. C. un poco por debajo del dólar a {mlc_value} pesos."
+            )
         else:
             text_output = f"Ni idea de lo que quieres decir compadre. No conozco ningún {currency_type}"
-        
+
         random_greating = get_random_greating()
-        speak_output = f'{random_greating}. {text_output}'
+        speak_output = f"{random_greating}. {text_output}"
 
         return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
+            handler_input.response_builder.speak(speak_output)
+            # .ask("add a reprompt if you want to keep the session open for the user to respond")
+            .response
         )
 
 
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
         return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
+    def handle(self, handler_input: HandlerInput) -> Response:
         speak_output = "Necesitas que te tire un cabo? Solo pregunta por las tasas de cambio de una moneda en específico y yo te tiro el dato asere."
 
         return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
+            handler_input.response_builder.speak(speak_output)
+            .ask(speak_output)
+            .response
         )
 
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
+    def can_handle(self, handler_input: HandlerInput) -> bool:
+        return ask_utils.is_intent_name("AMAZON.CancelIntent")(
+            handler_input
+        ) or ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input)
+
+    def handle(self, handler_input: HandlerInput) -> Response:
         speak_output = "Cuidate bro!"
 
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .response
-        )
+        return handler_input.response_builder.speak(speak_output).response
+
 
 class FallbackIntentHandler(AbstractRequestHandler):
     """Single handler for Fallback Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
         return ask_utils.is_intent_name("AMAZON.FallbackIntent")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
+    def handle(self, handler_input: HandlerInput) -> Response:
         logger.info("In FallbackIntentHandler")
         speech = "Hmm, No estoy seguro asere. Puedes decir Ayuda o preguntarme por cualquier moneda en Cuba. Dime que necesitas"
         reprompt = "No entendí un carajo. En qué quieres que te tire un cabo?"
 
         return handler_input.response_builder.speak(speech).ask(reprompt).response
 
+
 class SessionEndedRequestHandler(AbstractRequestHandler):
     """Handler for Session End."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
         return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-
+    def handle(self, handler_input: HandlerInput) -> Response:
         # Any cleanup logic goes here.
 
         return handler_input.response_builder.response
@@ -176,20 +163,18 @@ class IntentReflectorHandler(AbstractRequestHandler):
     for your intents by defining them above, then also adding them to the request
     handler chain below.
     """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
         return ask_utils.is_request_type("IntentRequest")(handler_input)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
+    def handle(self, handler_input: HandlerInput) -> Response:
         intent_name = ask_utils.get_intent_name(handler_input)
         speak_output = "You just triggered " + intent_name + "."
 
         return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
+            handler_input.response_builder.speak(speak_output)
+            # .ask("add a reprompt if you want to keep the session open for the user to respond")
+            .response
         )
 
 
@@ -198,22 +183,21 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
     stating the request handler chain is not found, you have not implemented a handler for
     the intent being invoked or included it in the skill builder below.
     """
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
+
+    def can_handle(self, handler_input: HandlerInput, exception: Exception) -> bool:
         return True
 
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
+    def handle(self, handler_input: HandlerInput, exception: Exception) -> Response:
         logger.error(exception, exc_info=True)
 
         speak_output = "Asere lo siento, tuve un problemilla ahí. Trata de nuevo."
 
         return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
+            handler_input.response_builder.speak(speak_output)
+            .ask(speak_output)
+            .response
         )
+
 
 # The SkillBuilder object acts as the entry point for your skill, routing all request and response
 # payloads to the handlers above. Make sure any new handlers or interceptors you've
@@ -229,7 +213,9 @@ sb.add_request_handler(ExchangeRateRequestIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(FallbackIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+sb.add_request_handler(
+    IntentReflectorHandler()
+)  # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 
 sb.add_exception_handler(CatchAllExceptionHandler())
 
